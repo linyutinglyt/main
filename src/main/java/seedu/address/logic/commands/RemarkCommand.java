a5d34ac61a551e7b9883934a896408d005bc4aee
linyutinglyt <e0332737@u.nus.edu>
2019-09-23 11:24:03 +0700

    checkstyle

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,10 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -7,10 +12,6 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 
-import java.util.List;
-
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 /**
  * Changes the remark of an existing person in the address book.
  */
@@ -19,13 +20,13 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified " +
-                    "by the index number used in the last person listing. "+
-                    "Existing remark will be overwritten by the input.\n"+
-                    "Parameters: INDEX (must be a positive integer) " +
-                    PREFIX_REMARK + "[REMARK]\n" +
-                    "Example: "+ COMMAND_WORD +" 1 " +
-                    PREFIX_REMARK + "Likes to swim.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+                    + "by the index number used in the last person listing. "
+                    + "Existing remark will be overwritten by the input.\n"
+                    + "Parameters: INDEX (must be a positive integer) "
+                    + PREFIX_REMARK + "[REMARK]\n"
+                    + "Example: " + COMMAND_WORD + " 1 "
+                    + PREFIX_REMARK + "Likes to swim.";
 
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
@@ -82,4 +83,5 @@ public class RemarkCommand extends Command {
         return index.equals(e.index)
                 && remark.equals(e.remark);
     }
+
 }
